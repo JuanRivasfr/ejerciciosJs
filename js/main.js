7d2142a4361cc43b41fc33b40057a42176ef7889
JuanRivasFr <156709972+JuanRivasfr@users.noreply.github.com>
2024-04-21 15:35:01 -0500

    feat: :sparkles: Finalizacion del ejercicio 4

--- js/main.js
+++ js/main.js
@@ -3,14 +3,13 @@ import { imprimirArreglo } from "./module/imprimirArreglo.js";
 
 let array = [];
 let isRun = true;
+let y = "";
 
 while (isRun) {
     let x = (await prompt("Digite un caracter para imprimir: "));
-    let y = "";
-    y += x;
+    array.push(x);
     let aux = (await prompt("Desea agregar otro caracter? (S/N): "));
     if (aux == "N") {
-        array.push(y);
         break;
     }
 }
